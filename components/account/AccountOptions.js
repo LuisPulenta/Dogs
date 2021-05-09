6011c7b008955bfd1bc6149042e5d9ab874b67e2
Luis Nuñez <luisalbertonu@gmail.com>
2021-05-09 14:04:28 -0300

    Video 59 Ok

--- components/account/AccountOptions.js
+++ components/account/AccountOptions.js
@@ -6,6 +6,7 @@ import Modal from '../Modal';
 
 import ChangeDisplayNameForm from './ChangeDisplayNameForm';
 import ChangeEmailForm from './ChangeEmailForm';
+import ChangePasswordForm from './ChangePasswordForm';
 
 export default function AccountOptions({user, toastRef,setReloadUser}) {
 
@@ -103,7 +104,11 @@ export default function AccountOptions({user, toastRef,setReloadUser}) {
                         break;
                 case "password":
                     setRenderComponent(
-                        <Text>password</Text>
+                        <ChangePasswordForm
+                            email={user.email}
+                            setShowModal={setShowModal}
+                            toastRef={toastRef}
+                            setReloadUser={setReloadUser}/>
                     )
                     break;
         }
