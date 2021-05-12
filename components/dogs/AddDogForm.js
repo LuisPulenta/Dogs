b6e3a0a66dcd2db381de5f5f7b843ade8beeebde
Luis Nuñez <luisalbertonu@gmail.com>
2021-05-12 11:03:03 -0300

    Video 68 Ok

--- components/dogs/AddDogForm.js
+++ components/dogs/AddDogForm.js
@@ -1,12 +1,265 @@
-import React from 'react'
-import { StyleSheet, Text, View } from 'react-native'
+import React, { useState } from 'react'
+import { Alert,Dimensions,StyleSheet, View,ScrollView } from 'react-native'
+import { Avatar,Input,Button,Icon,Image } from 'react-native-elements'
+import { map, size,filter } from 'lodash' 
+import CountryPicker from 'react-native-country-picker-modal'
 
-export default function AddDogForm({toastRef,setLoading}) {
+import {  loadImageFromGallery } from '../../utils/helpers'
+
+const widthScreen = Dimensions.get("window").width
+
+export default function AddDogForm({toastRef,setLoading,navigation}) {
+    const [formData, setFormData] = useState(defaultFormValues())
+    const [errorName, setErrorName] = useState(null)
+    const [errorDescription, setErrorDescription] = useState(null)
+    const [errorEmail, setErrorEmail] = useState(null)
+    const [errorAddress, setErrorAddress] = useState(null)
+    const [errorPhone, setErrorPhone] = useState(null)
+    const [imagesSelected, setImagesSelected] = useState([])
+
+    const addDog=()=>{
+        console.log(formData)
+        console.log("Fuck Yeah!!")
+    }   
+
     return (
-        <View>
-            <Text>AddDogForm...</Text>
+        <ScrollView style={styles.viewContainer}>
+            <ImageDog
+                imageDog={imagesSelected[0]}
+            />
+            <FormAdd
+                formData={formData}
+                setFormData={setFormData}
+                errorName={errorName}
+                errorDescription={errorDescription}
+                errorEmail={errorEmail}
+                errorAddress={errorAddress}
+                errorPhone={errorPhone}
+            />
+            <UploadImage
+                toastRef={toastRef}
+                imagesSelected={imagesSelected}
+                setImagesSelected={setImagesSelected}
+            />
+            <Button
+                title="Crear Raza de Perro"
+                onPress={addDog}
+                buttonStyle={styles.btnAddDog}
+            />
+        </ScrollView>
+    )
+}
+
+function ImageDog({ imageDog }) {
+    return (
+        <View style={styles.viewPhoto}>
+            <Image
+                style={{ width: widthScreen, height: 200}}
+                source={
+                    imageDog
+                        ? { uri: imageDog}
+                        : require("../../assets/no-image.png")
+                }
+            />
         </View>
     )
 }
 
-const styles = StyleSheet.create({})
+function UploadImage({toastRef,imagesSelected,setImagesSelected}) {
+    const imageSelect = async() => {
+        const response = await loadImageFromGallery([4, 3])
+        if (!response.status) {
+            toastRef.current.show("No has seleccionado ninguna imagen.", 3000)
+            return
+        }
+        setImagesSelected([...imagesSelected, response.image])
+    }
+
+    const removeImage = (image) => {
+        Alert.alert(
+            "Eliminar Imagen",
+            "¿Estas seguro que quieres eliminar la imagen?",
+            [
+                {
+                    text: "No",
+                    style: "cancel"                    
+                },
+                {
+                    text: "Sí",
+                    onPress: () => {
+                        setImagesSelected(
+                            filter(imagesSelected, (imageUrl) => imageUrl !== image)
+                        )
+                    }
+                }
+            ],
+            { cancelable: false }
+        )
+    }
+
+    return (
+        <ScrollView
+            horizontal
+            style={styles.viewImages}
+        >
+            {   
+                size(imagesSelected) < 10 && (
+                    <Icon
+                        type="material-community"
+                        name="camera"
+                        color="#7a7a7a"
+                        containerStyle={styles.containerIcon}
+                        onPress={imageSelect}
+                    />
+                )
+            }     
+            {
+                map(imagesSelected, (imageDog, index) => (
+                    <Avatar
+                        key={index}
+                        style={styles.miniatureStyle}
+                        source={{ uri: imageDog }}
+                        onPress={() => removeImage(imageDog)}
+                    />
+                ))
+            }     
+        </ScrollView>
+    )
+}
+
+function FormAdd({
+    formData, 
+    setFormData, 
+    errorName, 
+    errorDescription, 
+    errorEmail, 
+    errorAddress, 
+    errorPhone
+}) {
+    const [country, setCountry] = useState("AR")
+    const [callingCode, setCallingCode] = useState("54")
+    const [phone, setPhone] = useState("")
+
+    const onChange = (e, type) => {
+        setFormData({ ...formData, [type] : e.nativeEvent.text })
+    }
+
+    return (
+        <View style={styles.viewForm}>
+            <Input
+                placeholder="Nombre de la raza de perro..."
+                defaultValue={formData.name}
+                onChange={(e) => onChange(e, "name")}
+                errorMessage={errorName}
+            />
+            <Input
+                placeholder="Dirección..."
+                defaultValue={formData.address}
+                onChange={(e) => onChange(e, "address")}
+                errorMessage={errorAddress}
+            />
+            <Input
+                keyboardType="email-address"
+                placeholder="Email..."
+                defaultValue={formData.email}
+                onChange={(e) => onChange(e, "email")}
+                errorMessage={errorEmail}
+            />
+            <View style={styles.phoneView}>
+                <CountryPicker
+                    withFlag
+                    withCallingCode
+                    withFilter
+                    withCallingCodeButton
+                    containerStyle={styles.countryPicker}
+                    countryCode={country}
+                    onSelect={(country) => {
+                        setFormData({ 
+                            ...formData, 
+                            "country": country.cca2, 
+                            "callingCode": country.callingCode[0]
+                        })
+                    }}
+                            
+                />
+                <Input
+                    placeholder="WhatsApp..."
+                    keyboardType="phone-pad"
+                    containerStyle={styles.inputPhone}
+                    defaultValue={formData.phone}
+                    onChange={(e) => onChange(e, "phone")}
+                    errorMessage={errorPhone}
+                />
+            </View>
+            <Input
+                placeholder="Descripción de la raza..."
+                multiline
+                containerStyle={styles.textArea}
+                defaultValue={formData.description}
+                onChange={(e) => onChange(e, "description")}
+                errorMessage={errorDescription}
+            />
+        </View>
+    )
+
+
+ }
+
+ const defaultFormValues = () => {
+    return {
+        name: "",
+        description: "",
+        email: "",
+        phone: "",
+        address: "",
+        country: "AR",
+        callingCode: "54"
+    }
+}
+
+const styles = StyleSheet.create({
+    viewContainer: {
+        height: "100%"
+    },
+    viewForm: {
+        marginHorizontal: 10,
+    },
+    textArea: {
+        height: 100,
+        width: "100%"
+    },
+    phoneView: {
+        width: "80%",
+        flexDirection: "row"
+    },
+    inputPhone: {
+        width: "80%"
+    },
+    btnAddDog: {
+        margin: 20,
+        backgroundColor: "#a42424"
+    },
+    viewImages: {
+        flexDirection: "row",
+        marginHorizontal: 20,
+        marginTop: 30
+    },
+    containerIcon: {
+        alignItems: "center",
+        justifyContent: "center",
+        marginRight: 10,
+        height: 70,
+        width: 79,
+        backgroundColor: "#e3e3e3"
+    },
+    miniatureStyle: {
+        width: 70,
+        height: 70,
+        marginRight: 10
+    },
+    viewPhoto: {
+        alignItems: "center",
+        height: 200,
+        marginBottom: 20
+    }
+})
