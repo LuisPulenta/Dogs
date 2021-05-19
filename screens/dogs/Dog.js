9f42065644584b33329ec5637a7613f43895e3d4
Luis Nuñez <luisalbertonu@gmail.com>
2021-05-19 14:57:46 -0300

    Video 86 Ok

--- screens/dogs/Dog.js
+++ screens/dogs/Dog.js
@@ -1,13 +1,19 @@
 import React, { useState, useEffect } from 'react'
-import { Alert,StyleSheet, Text, View } from 'react-native'
+import { Alert,Dimensions,StyleSheet, Text, ScrollView,View } from 'react-native'
 import { getDocumentById} from '../../utils/actions'
-
+import { Icon,ListItem,Rating } from 'react-native-elements'
+import { map } from 'lodash'
+import CarouselImages from '../../components/CarouselImages'
 import Loading from '../../components/Loading'
+import MapDog from '../../components/dogs/MapDog'
+import { formatPhone } from '../../utils/helpers'
+
+const widthScreen = Dimensions.get("window").width
 
 export default function Dog({ navigation, route }) {
     const { id, name } = route.params
     const [dog, setDog] = useState(null)
-
+    const [activeSlide, setActiveSlide] = useState(0)
     navigation.setOptions({ title: name })
 
     useEffect(() => {
@@ -28,11 +34,128 @@ export default function Dog({ navigation, route }) {
     
 
     return (
-        <View>
-            <Text>{dog.name}</Text>
-            <Text>{dog.description}</Text>
+        <ScrollView style={styles.viewBody}>
+            <CarouselImages
+                images={dog.images}
+                height={250}
+                width={widthScreen}
+                activeSlide={activeSlide}
+                setActiveSlide={setActiveSlide}
+            />
+            <TitleDog
+                name={dog.name}
+                description={dog.description}
+                rating={dog.rating}
+            />
+            <DogInfo
+                name={dog.name}
+                location={dog.location}
+                address={dog.address}
+                email={dog.email}
+                phone={formatPhone(dog.callingCode, dog.phone)}
+            />
+        </ScrollView>
+        
+    )
+}
+
+function TitleDog({ name, description, rating }) {
+    return (
+        <View style={styles.viewDogTitle}>
+            <View style={styles.viewDogContainer}>
+                <Text style={styles.nameDog}>{name}</Text>
+                <Rating
+                    style={styles.rating}
+                    imageSize={20}
+                    readonly
+                    startingValue={parseFloat(rating)}
+                />
+            </View>
+            <Text style={styles.descriptionDog}>{description}</Text>
         </View>
     )
 }
 
-const styles = StyleSheet.create({})
+function DogInfo({ 
+    name, 
+    location, 
+    address, 
+    email, 
+    phone
+}) {
+    const listInfo = [
+        { type: "addres", text: address, iconName: "map-marker" },
+        { type: "phone", text: phone, iconName: "phone"},
+        { type: "email", text: email, iconName: "at" },
+    ]
+
+    return (
+        <View style={styles.viewDogInfo}>
+            <Text style={styles.dogInfoTitle}>
+                Información sobre la raza canina
+            </Text>
+            <MapDog
+                location={location}
+                name={name}
+                height={150}
+            />
+            {
+                map(listInfo, (item, index) => (
+                    <ListItem
+                        key={index}
+                        style={styles.containerListItem}
+                    >
+                        <Icon
+                            type="material-community"
+                            name={item.iconLeft}
+                            color="#a42424"
+                            onPress={() => actionLeft(item.type)}
+                        />
+                        <ListItem.Content>
+                            <ListItem.Title>{item.text}</ListItem.Title>
+                        </ListItem.Content>
+                    </ListItem>
+                ))
+            }
+        </View>
+    )
+}
+
+const styles = StyleSheet.create({
+    viewBody: {
+        flex: 1,
+        backgroundColor: "#fff"
+    },
+    viewDogTitle: {
+        padding: 15,
+    },
+    viewDogContainer: {
+        flexDirection: "row"
+    },
+    descriptionDog: {
+        marginTop: 8,
+        color: "gray",
+        textAlign: "justify"
+    },
+    rating: {
+        position: "absolute",
+        right: 0
+    },
+    nameDog: {
+        fontWeight: "bold",
+        color: "blue"
+    },
+    viewDogInfo: {
+        margin: 15,
+        marginTop: 25
+    },
+    dogInfoTitle: {
+        fontSize: 20,
+        fontWeight: "bold",
+        marginBottom: 15
+    },
+    containerListItem: {
+        borderBottomColor: "#c27c5d",
+        borderBottomWidth: 1
+    }
+})
